3aba9748da7b76ec5b274506de7dd9513dae1e66
Glorp <jonas.winje@gmail.com>
2014-11-13 18:50:10 +0100

    links-and-things

--- Ting.elm
+++ Ting.elm
@@ -5,9 +5,49 @@ import String
 import Maybe
 import Http
 import Json
-
+import Dict
+import Color
 
 id x = x
+
+bordered e = let (x, y) = sizeOf e
+                 c      = color Color.white (container (x + 4) (y + 4) middle e)
+             in color Color.darkGrey (container (x + 8) (y + 8) middle c)
+
+renderJson : Json.Value -> Element
+renderJson x = case x of
+                   Json.String s  -> plainText s
+                   Json.Number n  -> plainText (show n)
+                   Json.Boolean b -> plainText (show b)
+                   Json.Null      -> plainText "null"
+                   Json.Array l   -> flow right (join [plainText ", "] [map renderJson l])
+                   Json.Object d  -> let render (k, v) = flow right [plainText k,
+                                                                     plainText ": ",
+                                                                     renderJson v]
+                                     in bordered (flow down (map render (Dict.toList d)))
+
+
+renderStuff j l = let renderD d l = case l of
+                                        []           -> renderJson (Json.Object d)
+                                        (s, f) :: xs -> (case Dict.get s d of
+                                                             Just j  -> above (f j) (renderD (Dict.remove s d) xs)
+                                                             Nothing -> renderD d xs)
+                  in case j of
+                         Json.Object d -> renderD d l
+                         _             -> renderJson j
+
+renderLink j = case j of
+                   Json.Object d -> (case (Dict.get "href" d) of
+                                         Just (Json.String s)  -> beside (renderJson (Json.Object d))
+                                                                         (Input.button handle (Just (Http.get s)) "boop")
+                                         _                     -> renderJson j)
+                   _             -> renderJson j
+
+renderLinks j = case j of
+                    Json.Array l -> flow down (map renderLink l)
+                    _            -> renderJson j
+
+
 
 makeField s ss = let content = Input.input (Field.Content ss (Field.Selection 0 0 Field.Forward))
                      fi = lift (Field.field Field.defaultStyle content.handle id s) content.signal
@@ -15,16 +55,17 @@ makeField s ss = let content = Input.input (Field.Content ss (Field.Selection 0 
 
 binput = Input.input ()
 
-b = Input.button binput.handle () "Knapp"
+b = Input.button binput.handle () "beep"
 
-(field, handle, reqSig) = let inp = Input.input Nothing
-                              (field, content) = makeField "λ" "meh.txt"
+(field, handle, reqSig) = let addr = "meh.txt"
+                              inp = Input.input Nothing
+                              (field, content) = makeField "Thing" addr
                               fieldSig = sampleOn binput.signal content
-                              fieldReqSig = lift (.string >> Http.get) fieldSig
-                              foo fieldV v = case v of
-                                                 Just s  -> s
-                                                 Nothing -> fieldV
-                              reqSig = lift2 foo fieldReqSig inp.signal
+                              fieldReqSig = lift (.string >> Http.get >> Just) fieldSig
+                              mergef mReq req = case mReq of
+                                                    Just r  -> r
+                                                    Nothing -> req
+                              reqSig = foldp mergef (Http.get addr) (merge fieldReqSig inp.signal)
                           in (field, inp.handle, reqSig)
 
 
@@ -36,18 +77,14 @@ respToStr x = case x of
                  Http.Waiting     -> ":|"
                  Http.Failure i s -> String.append ":((( " s
 
+strToGUI s = case Json.fromString s of
+                 Just x -> renderStuff x [("links", renderLinks)]
+                 _      -> plainText (String.concat ["not soap? :() ", s])
+
 port out : Signal String
 port out = lift respToStr res
 
-port inn : Signal String
+port inn : Signal Json.Value
 
-strToGUI s = let foo x = case x of
-                        Json.String s -> Input.button handle (Just (Http.get s)) s
-                        _             -> plainText ":/"
-        in case Json.fromString s of
-               Just (Json.Array x) -> flow down (map foo x)
-               _                   -> plainText (String.concat [":( -- ", s])
-
-
-main = lift (flow down) (combine [lift (flow right) (combine [field, constant b, lift (respToStr >> strToGUI) res]),
-                                  lift plainText inn])
+main = lift (flow down) (combine [lift (flow right) (combine [field, constant b]),
+                                  lift (flow right) (combine [lift (respToStr >> strToGUI) res])])
