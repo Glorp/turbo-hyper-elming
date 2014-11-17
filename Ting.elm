5c208cb1dc99e382520e5dfc85355dc4dd2aea08
Jonas <jonas.winje@gmail.com>
2014-11-17 18:38:50 +0100

    less more broken actionthings

--- Ting.elm
+++ Ting.elm
@@ -32,25 +32,25 @@ port out = reqSig
 
 port inn : Signal (Maybe Response)
 
-strToGUI : String -> Element
-strToGUI s =
+strToGUI : String -> Dict.Dict String (Dict.Dict String Field.Content) -> Element
+strToGUI s fs =
     let renderl = [("properties", Gfx.renderJsonBut [("name", Gfx.renderJson),
                                                      ("description", Gfx.renderJson)]),
                    ("links", renderLinks),
-                   ("actions", Gfx.renderJson)]
+                   ("actions", renderActions fs)]
     in case Json.fromString s of
            Just x -> Gfx.renderJsonBut renderl x
            _      -> plainText (concat ["not soap? :() ", s])
 
-respToGUI : Maybe Response -> Element
-respToGUI x =
+respToGUI : Maybe Response -> Dict.Dict String (Dict.Dict String Field.Content) -> Element
+respToGUI x fs =
     case x of
         Nothing -> plainText "..."
         Just r  -> flow down [(plainText (concat ["Status: " , show (r.status), ", ", r.statusText])),
                                          (case r.headers of
                                               Nothing -> empty
                                               Just h  -> plainText h),
-                                         strToGUI r.respText]
+                                         strToGUI r.respText fs]
 
 renderLink : Json.Value -> Element
 renderLink j =
@@ -77,43 +77,46 @@ actionFieldInp = Input.input Nothing
 
 actionFieldSig = let foo x d = case x of
                                    Nothing        -> Dict.empty
-                                   Just (f, c) -> Dict.insert f c d
+                                   Just (a, f, c) -> insContent a f c d
                  in foldp foo Dict.empty (merge actionFieldInp.signal (lift (\_ -> Nothing) reqSig))
 
-getContent k d = case Dict.get k d of
-                          Just x -> x
-                          _      -> Field.Content "" (Field.Selection 0 0 Field.Forward)
+getContent a f d =
+    let defaultC = Field.Content "" (Field.Selection 0 0 Field.Forward)
+    in case Dict.get a d of
+           Just x -> (case Dict.get f x of
+                          Just c -> c
+                          _      -> defaultC)
+           _      -> defaultC
 
+insContent a f c d = case Dict.get a d of
+                          Just dd -> Dict.insert a (Dict.insert f c dd) d
+                          _       -> Dict.insert a (Dict.singleton f c) d
+
+renderAction : Dict.Dict String (Dict.Dict String Field.Content) -> Json.Value -> Element
 renderAction fs j =
   let content = Field.Content "" (Field.Selection 0 0 Field.Forward)
-      foo f x = Just (f, x)
+      foo a f x = Just (a, f, x)
       rendField name f = case f of
                              Json.Object d -> (case Dict.get "name" d of
-                                                   Just (Json.String s) -> beside (plainText (concat [s, ": "]))
-                                                                                  (Field.field Field.defaultStyle actionFieldInp.handle (foo (name, s)) "" (getContent (name, s) fs))
-                                                   _                    -> plainText "rendField-inner, nope :(")
-                             _                  -> plainText "rendField, nope :("
+                                                   Just (Json.String s) -> (plainText (concat [s, ": "]),
+                                                                            Field.field Field.defaultStyle actionFieldInp.handle (foo name s) "" (getContent name s fs))
+                                                   _                    -> (plainText "rendField-inner, nope :(", empty))
+                             _                  -> (plainText "rendField, nope :(", empty)
       rend d = case (Dict.get "name" d, Dict.get "fields" d) of
-                   (Just (Json.String s), Just (Json.Array l))  -> Gfx.bordered Color.darkGray (flow down (plainText s :: (map (rendField s) l)))
+                   (Just (Json.String s), Just (Json.Array l))  -> Gfx.bordered Color.darkGray (above (plainText s) (Gfx.renderKV (map (rendField s) l)))
                    _                                            -> plainText "rend, nope :("
   in case j of
          Json.Object d -> beside (rend d) (Input.button handle Nothing "boop")
          _             -> plainText "renderAction, nope :("
 
-renderActions r fs =
-    let rend s = case Json.fromString s of
-                     Just (Json.Object d) -> (case Dict.get "actions" d of
-                                                  Just (Json.Array l) -> flow down (map (renderAction fs) l)
-                                                  _                   -> plainText "renderActions, inner, inner, nope :(")
-                     _                              -> plainText "renderActions, inner, nope :("
-    in case r of
-           Just resp -> rend resp.respText
-           _         -> empty
+renderActions : Dict.Dict String (Dict.Dict String Field.Content) -> Json.Value -> Element
+renderActions fs j =
+    case j of
+        Json.Array l -> flow down (map (renderAction fs) l)
+        _            -> plainText "renderActions, nope :("
 
 main : Signal Element
 main = let hed  = lift (flow right) (combine [field, constant b])
-           stuf = lift (flow right) (combine [lift respToGUI inn])
-           acts = lift2 renderActions inn actionFieldSig
+           stuf = lift2 respToGUI inn actionFieldSig
        in lift (flow down) (combine [lift2 Gfx.centered Window.width hed,
-                                     lift2 Gfx.centered Window.width stuf,
-                                     lift2 Gfx.centered Window.width acts])
+                                     lift2 Gfx.centered Window.width stuf])
